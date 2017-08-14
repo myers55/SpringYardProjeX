bb82c916f4b5d7049f70a337e2abbe85267d3013
Dylan Myers <sunshine@Dylans-MacBook-Air.local>
2017-08-14 10:35:36 -0400

    first commit

--- src/main/java/com/example/customer/Controllers/CustomersController.java
+++ src/main/java/com/example/customer/Controllers/CustomersController.java
@@ -4,6 +4,8 @@ import com.example.customer.Model.Customer;
 import com.example.customer.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -59,4 +61,14 @@ public class CustomersController {
 //        CustomerService response = restTemplate.getForObject(route, CustomerService.class);
 //        return response.getById(Customer);
 //    }
+private void showPrinciple() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    System.out.println("\n\n\n ===========>");
+    System.out.println("name " + authentication.getName());
+    System.out.println("details " + authentication.getDetails());
+    System.out.println("authorties " + authentication.getAuthorities());
+    System.out.println("credentials " + authentication.getCredentials());
+    System.out.println("principal " + authentication.getPrincipal());
+    System.out.println("is authenticated " + authentication.isAuthenticated());
+}
 }
